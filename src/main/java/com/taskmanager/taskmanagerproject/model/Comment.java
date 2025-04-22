63e4477aa52f4c3e0d162862e73bff66b1001a77
danielbirsan <75643437+danielbirsan@users.noreply.github.com>
2025-04-22 22:14:42 +0300

    add models

--- src/main/java/com/taskmanager/taskmanagerproject/model/Comment.java
+++ src/main/java/com/taskmanager/taskmanagerproject/model/Comment.java
@@ -1,4 +1,34 @@
 package com.taskmanager.taskmanagerproject.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@Entity
 public class Comment {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "content")
+    private String content;
+
+    @Builder.Default
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
 }
+
--- src/main/java/com/taskmanager/taskmanagerproject/model/Task.java
+++ src/main/java/com/taskmanager/taskmanagerproject/model/Task.java
@@ -1,4 +1,46 @@
 package com.taskmanager.taskmanagerproject.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@ToString(exclude = { "subtasks", "comments"})
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+    @Column(name = "responsible")
+    private String responsible;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Task parent;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Task> subtasks = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+
 }
