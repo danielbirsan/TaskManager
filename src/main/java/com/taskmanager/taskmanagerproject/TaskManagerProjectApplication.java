419adc0a4d4a943a2227824f226d8564629083a6
danielbirsan <75643437+danielbirsan@users.noreply.github.com>
2025-04-22 21:09:38 +0300

    initialize Spring Boot project with Maven setup and basic structure

---
+++ src/main/java/com/taskmanager/taskmanagerproject/TaskManagerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.taskmanagerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taskmanager/taskmanagerproject/TaskManagerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.taskmanagerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
